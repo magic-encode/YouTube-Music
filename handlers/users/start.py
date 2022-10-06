904917c22951ee6612ea964c0b4285f43bc99b07
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-10-06 16:40:29 +0500

    Fixed start commands

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,3 +7,4 @@ from loader import dp
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer("Bu bot YouTubedan qoshiq yuklab beruvchi bot. \nLink jonting biz musiqasini tashlab bermaiz.!")
